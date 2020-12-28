d7461d76346ad59821a9f79e92ac1b96e09e0c51
Ваше Имя <you@example.com>
2020-12-28 20:07:04 +0300

    add weight and brand

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 import pandas as pd
 import re
-import xlwt
-import time
-
-"""'Бренд из НС': ['None' for i in range(9999)],
-                           'Объем/вес из НС': ['None' for i in range(9999)],
-                           'Наименование полное (НП)': ['None' for i in range(9999)],
-                           'Наименование товара из НП': ['None' for i in range(9999)],
-                           'Бренд из НП': ['None' for i in range(9999)],
-                           'Объем/вес из НП': ['None' for i in range(9999)]"""
 
 
 class Convert:
@@ -24,59 +15,122 @@ class Convert:
         file = pd.read_excel(self.template_file, sheet_name='result_conv2')
         return file.iloc[:, (columns-1):columns]
 
-    def unnecessary_name(self):
+    def unnecessary_weight(self):
         file_convert = self.init_converible_file()
-        file_tamplates = self.init_templates_file(3)
+        pattern = open('data/re_search_weight.txt', 'r').read()
+        repl = r''
+        tmp_mass = []
+        save_info = []
+        for field in file_convert:
+            flag = False
+            for tmp in pattern.split('\n'):
+                if field and tmp and re.search(tmp, str(field)):
+                    tmp_mass.append(re.sub(tmp, repl, str(field)).strip())
+                    save_info.append(re.search(tmp, str(field)))
+                    flag = True
+                    break
+            if not flag:
+                tmp_mass.append(str(field))
+                save_info.append('')
 
+        for match in range(len(save_info)):
+            if save_info[match]:
+                save_info[match] = save_info[match].group(0)
+        return tmp_mass, save_info
 
-    def unnecessary(self):
+    def unnecessary_brand(self):
+        file_del_weight, mass = self.unnecessary_weight()
+        file_del_weight = [str(i).lower() for i in file_del_weight]
+        pattern = open('data/re_search_brand.txt', 'r').read()
+        repl = r''
+        tmp_mass = []
+        save_info = []
+        for field in file_del_weight:
+            flag = False
+            for tmp in pattern.split('\n'):
+                if field and tmp and re.search(tmp, str(field)):
+                    tmp_mass.append(re.sub(tmp, ' ' + repl, str(field)).strip())
+                    save_info.append(re.search(tmp, str(field)))
+                    flag = True
+                    break
+            if not flag:
+                tmp_mass.append(str(field))
+                save_info.append('')
+        for match in range(len(save_info)):
+            if save_info[match]:
+                save_info[match] = save_info[match].group(0)
+        return tmp_mass, save_info
+
+    def delete_unnecessary_symbol(self):
+        mass_product, mass_brand = self.unnecessary_brand()
+        mass_pattern = open('data/re_delete_unsymbol.txt').read()
+        new_mass_product = []
+        for field in mass_product:
+            string = field
+            for pattern in mass_pattern.split('\n'):
+                string = re.sub(pattern, '', str(string)).strip()
+            new_mass_product.append(string)
+        return new_mass_product
+
+    def to_full_weight(self):
+        mass_name_prod, mass_weight = self.unnecessary_weight()
+        patterns = open('data/re_full_weight.txt').read()
+        mass_full_weight = []
+        for field in mass_weight:
+            string = field
+            for pattern in patterns.split('\n'):
+                if pattern:
+                    pattern = pattern.split('|||')
+                    if re.search(pattern[0], string):
+                        string = re.sub(pattern[0], r'\1' + pattern[1], string)
+                        break
+            mass_full_weight.append(string)
+        return mass_full_weight
+
+    def to_full_brand(self):
+        mass_name_prod, mass_brand = self.unnecessary_brand()
+        patterns = open('data/re_full_brand.txt').read()
+        mass_full_brand = []
+        for field in mass_brand:
+            string = field
+            for pattern in patterns.split('\n'):
+                if pattern:
+                    pattern = pattern.split('|||')
+                    if re.search(pattern[0], string):
+                        string = re.sub(pattern[0], pattern[1], string).strip()
+                        break
+            mass_full_brand.append(string)
+        return mass_full_brand
+
+    def create_list_xls(self):
         file_convert = self.init_converible_file()
-        file_tamplates = self.init_templates_file(3)
         mass = []
         for i in file_convert:
             mass.append(i)
+        return mass
 
-        pattern = r'([*#]\d{2,3})|(\d{1,3}[ ][Кк][Гг])|' \
-                  r'(\d{1,4}[ш][т])|(\d{1,3}[Мм][Лл])|((\d{1,3}[Х])\d{1,3}[ГгКк]$)|(\d{1,3}\w+[^%]\:\d)|' \
-                  r'(\d\,\d[Гг])|(\d{1,3}[Кк][Гг])|(\s{2}\d$)|([(]\d{1,2}[+]\d{1,2}[)])|([#*])|(\d{1,4}$)|' \
-                  r'(\d{1,3}[,.]\d{1,3}[LlЛл]|(\d{1,3}[LlЛл]))|(\d{1,2}[+])|(\d{1,3}[*]\d{1,3}[*]\d{1,3}[Мм]{1,2}[,])' \
-                  r'|(\d{1,4}([ ])[МмШшCc][ЛлТт])|([Кк][Гг]$)|([:]\d{1,3}\/\d{1,3})|([:]\d{1,3}[.,]\d{1,4})|(\d{1,4}[-:]\d{1,4}([Кк][Гг]))|(\d{1,3}[Гг])|' \
-                  r'(\d{2,3}\s([Гг]))|(\d{1}[,]\d{1}[^%]([Лл]))|(\d{1,3}[Гг][Рр])|(\d{1,3}[,.]\d{1,3}$)|(\d{1,4}[Гг]$)|(\d{1,3}[.]$)' \
-                  r'|(\d{1,3}[Х]\d{1,3}[Гг][Рр])|([(]$)|(\d{2,3})|(\d{1,4} [МмШшCc][ЛлТт][,.])|(\d{1,3}[Кк])'
-        repl = r''
-        mass3 = []
-        for i in file_convert:
-            mass3.append(re.sub(pattern, repl, str(i)).strip())
-
-        mass3tmp = [str(i).lower() for i in mass3]
-        template_tmp = [str(i).lower() for i in file_tamplates['Наименование товара из НС']]
-        mass2 = []
-        c = 0
-        for i in range(len(mass3tmp)):
-            if mass3tmp[i] in template_tmp:
-                mass2.append(mass3[i])
-                c += 1
-            else:
-                mass2.append('')
-        for i in range(len(mass2)):
-            mass2[i] = str(mass2[i]).lower()
-        open('names.txt', 'w').write('\n'.join(set(mass2)).lower())
-        open('names_all.txt', 'w').write('\n'.join(set(template_tmp)))
-
-        mydict = {'Наименование сокращенное (НС)': mass,
-                  'Наименование товара из НС': mass2}
+    def create_dataframe(self):
+        name_ns, name_weight = self.unnecessary_weight()
+        brand_ns, name_brand = self.unnecessary_brand()
+        name_product = self.delete_unnecessary_symbol()
+        mass_full_weight = self.to_full_weight()
+        mass_full_brand = self.to_full_brand()
+        mydict = {'Наименование сокращенное (НС)': self.create_list_xls(),
+                  'Наименование товара': name_product,
+                  'бренд из НС': name_brand,
+                  'Вес': name_weight,
+                  'Бренд без сокращений': mass_full_brand,
+                  'Вес без сокращений': mass_full_weight}
         df = pd.DataFrame({key: pd.Series(value) for key, value in mydict.items()})
         return df
 
     def create_new_xls(self):
-        open('test.xls', 'w')
-        df = self.unnecessary()
-        df.to_excel('data/test.xls', sheet_name='Лист 1', index=False)
+        open('result_table.xls', 'w')
+        df = self.create_dataframe()
+        df.to_excel('data/result_table.xls', sheet_name='Лист 1', index=False)
         return print('OK')
 
 
 if __name__ == "__main__":
     a = Convert()
     a.create_new_xls()
-    #a.unnecessary()
-    #print(a.init_templates_file(3))
